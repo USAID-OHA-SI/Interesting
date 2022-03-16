39f1e89e43c3e89c17be77ac17a1637c9893e0ed
Srikanth <krs8rb@virginia.edu>
2022-03-16 14:41:38 -0400

    template column names saved as object - attempting mapping function for wide templates

--- data-raw/template_columns.R
+++ data-raw/template_columns.R
@@ -8,10 +8,25 @@ template_cols_long <- readxl::read_excel("data-raw/templates/FY22_CIRG_Submissio
 usethis::use_data(template_cols_long, overwrite = TRUE)
 
 #store column names for semi_wide template
-  template_cols_semiwide <- readxl::read_excel("data-raw/templates/FY22_CIRG_Submission_Semi_Wide_ All_Technical_Areas.xlsx",
-                                           sheet = "CIRG", col_types = "text", n_max = 0) %>%
+  # template_cols_semiwide <- readxl::read_excel("data-raw/templates/FY22_CIRG_Submission_Semi_Wide_ All_Technical_Areas.xlsx",
+  #                                          sheet = "CIRG", col_types = "text", n_max = 0) %>%
+  #   names()
+
+  template_cols_semiwide <- readxl::excel_sheets("data-raw/templates/FY22_CIRG_Submission_Semi_Wide_ All_Technical_Areas.xlsx") %>%
+    #setdiff("meta") %>%
+    stringr::str_subset("CIRG") %>%
+    purrr::map_dfr(.f = ~ readxl::read_excel("data-raw/templates/FY22_CIRG_Submission_Semi_Wide_ All_Technical_Areas.xlsx", sheet = .x, skip = 2, col_types = "text")) %>%
     names()
 
+  #Wide Key POp
+
+  template_cols_wide_kp <- readxl::excel_sheets("data-raw/templates/FY22 CIRG Submission - Wide - KEY POPULATIONS.xlsx") %>%
+    #setdiff("meta") %>%
+    stringr::str_subset("CIRG") %>%
+    purrr::map_dfr(.f = ~ readxl::read_excel("data-raw/templates/FY22 CIRG Submission - Wide - KEY POPULATIONS.xlsx", sheet = .x, skip = 2, col_types = "text")) %>%
+    names()
+
+  #-----
 
   semiwide_sheet <- readxl::excel_sheets("data-raw/templates/FY22_CIRG_Submission_Semi_Wide_ All_Technical_Areas.xlsx")[2:9]
 
