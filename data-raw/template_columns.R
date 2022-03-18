7e02ffe084075b9cae1c74bb833f20d7dbc5e12d
Srikanth <krs8rb@virginia.edu>
2022-03-18 15:43:28 -0400

    added loop to read in wide templates

--- data-raw/template_columns.R
+++ data-raw/template_columns.R
@@ -15,31 +15,53 @@ usethis::use_data(template_cols_long, overwrite = TRUE)
     purrr::map_dfr(.f = ~ readxl::read_excel("data-raw/templates/FY22_CIRG_Submission_Semi_Wide_ All_Technical_Areas.xlsx", sheet = .x, skip = 2, col_types = "text")) %>%
     names()
 
-  #Store column names for wide template -------------------
-
-  #Store path
+ #Store column names for wide template -------------------
 
   template_path <- "data-raw/templates/Wide - by Technical Area/"
 
-  template_files <- dir({template_path}, pattern = "*.xlsx")
+  files <- dir({template_path}, pattern = "*.xlsx", full.names = TRUE)
 
-  #loop through and grab names
-  temp_names <- template_files %>%
-    purrr::map(~readxl::excel_sheets(file.path(template_path, .))) %>%
-    purrr::map(~stringr::str_subset(., "CIRG"))
+  df_tabs <- map_dfr(files,
+                     ~ tibble(filename = .x,
+                              tabname = map(filename, readxl::excel_sheets)) %>%
+                       flatten()) %>%
+    filter(tabname != "meta")
 
-  #map dfr to get template names for each wide template
-  purrr::map2_dfr(.y = glue::glue("{template_path}{template_files}"), .x = temp_names, .f = ~ readxl::read_excel(.y, sheet = .x, skip = 2, col_types = "text")) %>%
-    names()
+  #type of template
+  tab_type <- purrr::map(.x = files, ~tail(strsplit(.x,split=" ")[[1]],1)) %>%
+    unlist()
+
+  tab_type <- tab_type %>% stringr::str_extract("[^.]+")
 
 
+  #loop
 
-  #KP only
-  template_cols_wide_kp <- readxl::excel_sheets("data-raw/templates/FY22 CIRG Submission - Wide - KEY POPULATIONS.xlsx") %>%
-    #setdiff("meta") %>%
-    stringr::str_subset("CIRG") %>%
-    purrr::map_dfr(.f = ~ readxl::read_excel("data-raw/templates/FY22 CIRG Submission - Wide - KEY POPULATIONS.xlsx", sheet = .x, skip = 2, col_types = "text")) %>%
-    names()
+  lst <- as.list(rep(NA, length(tab_type)))
+  lst2 <- as.list(rep(NA, length(tab_type)))
+
+  for (i in 1:length(tab_type)) {
+    lst[[i]] <- df_tabs %>%
+      filter(str_detect(filename, tab_type[i]))
+
+    lst2[[i]] <-  lst[[i]] %>%
+      pmap_dfr(.f = ~readxl::read_excel(..1, ..2)) %>%
+      names()
+
+  }
+
+#can I use glue?
+
+ # glue("temp_wide_{tab_type[2]}") <- lst2[[2]]
+
+ temp_wide_dreams <- lst2[[1]]
+ temp_wide_gender <- lst2[[2]]
+ temp_wide_kp <- lst2[[3]]
+ temp_wide_lab <- lst2[[4]]
+ temp_wide_ovc <- lst2[[5]]
+ temp_wide_prep <- lst2[[6]]
+ temp_wide_sch <- lst2[[7]]
+ temp_wide_vmmc <- lst2[[8]]
+
 
   #-----
 
