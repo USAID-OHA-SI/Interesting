d76dedae1183183eb9940c809169b9ef1d92bf48
Srikanth <krs8rb@virginia.edu>
2022-03-29 13:53:10 -0400

    added col conditions for all 8 wide templates

--- R/validate_import.R
+++ R/validate_import.R
@@ -11,8 +11,22 @@ validate_import <- function(df){
     req_cols <- template_cols_long
   } else if(var_exists(df, "dreams_fp.....")){
     req_cols <- template_cols_semiwide
+  } else if(var_exists(df, "dreams_fp.15_19.f...d")){
+    req_cols <- temp_wide_dreams
+  } else if(var_exists(df, "gend_gbv.10_14.f.sexualviolence..n")){
+    req_cols <- temp_wide_gender
   } else if(var_exists(df, "tx_new_verify....fsw.n")){
-    req_cols <- template_cols_wide_kp
+    req_cols <- temp_wide_kp
+  } else if(var_exists(df, "pmtct_eid_eligible.0_12m..eideligible..n")){
+    req_cols <- temp_wide_lab
+  } else if(var_exists(df, "ovc_offer.u1.m...n")){
+    req_cols <- temp_wide_ovc
+  } else if(var_exists(df, "prep_screen.10_14.m...n")){
+    req_cols <- temp_wide_prep
+  } else if(var_exists(df, "sc_arvdisp...tld30_countbottles..n")){
+    req_cols <- temp_wide_sch
+  } else if(var_exists(df, "vmmc_ae.20_24.male...n")){
+    req_cols <- temp_wide_vmmc
   }
 
   missing <- flag_missing(req_cols, names(df))
