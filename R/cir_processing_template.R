59658aca8c4fd2ced17679b0b199d4492035e52e
Srikanth <krs8rb@virginia.edu>
2022-04-29 16:35:05 -0400

    pushing updated template

--- R/cir_processing_template.R
+++ R/cir_processing_template.R
@@ -1,10 +1,13 @@
 #validation checks
 validate_initial(filepath)
 
+#store meta df - come back to google ID
+meta_df <- cir_store_meta(filepath)
+
 #import template sheet(s) - this is being being built locally right now (can we do this from drive and use the filename to identify)
   #in order for this to work, we need the naming conventions to be consistent
 df <- cir_import(filepath)
-
+s
 #validation checks - VMMC doesnt work on this one because of the names issue - come back to that!
 validate_import(df)
 
