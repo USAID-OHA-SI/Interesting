d5d9b8c1e8586afcf326cb605c77bca07a17fd68
Srikanth <krs8rb@virginia.edu>
2022-05-22 08:00:53 -0400

    attempt to resolve vmmc

--- R/cir_import.R
+++ R/cir_import.R
@@ -17,7 +17,10 @@ cir_import <- function(filepath){
     #setdiff("meta") %>%
     stringr::str_subset("CIRG") %>%
     purrr::map_dfr(.f = ~ readxl::read_excel(filepath, sheet = .x, skip = 2, col_types = "text"))
-
+  # %>%
+  #   rename('vmmc_ae..m.sitetype:unknown..n' = vmmc_ae..m.unknown..n...30,
+  #          'vmmc_ae..m.aetype:unknown..n' = vmmc_ae..m.unknown..n...20)
+  #
   if("mechanismid" %in% names(df))
     df <- dplyr::rename(df, mech_code = mechanismid)
 
