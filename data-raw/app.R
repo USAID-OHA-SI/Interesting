c750158e993d0bf7ef54efa48a87f13040698bd7
Baboyma Kagniniwa <baboyma@users.noreply.github.com>
2022-11-18 14:36:23 -0500

    Validation app - upload + initial validation

--- data-raw/app.R
+++ data-raw/app.R
@@ -1,12 +1,20 @@
 library(shiny)
 library(shinythemes)
 library(tidyverse)
+library(glamr)
+library(glitr)
+library(Interesting)
+library(googledrive)
+library(googlesheets4)
+library(glue)
+
 
 
 # Global Options ----
 
   app_title <- "FEPFAR/USAID - Custom Indicators Reporting"
-  nav_title <- "CIRG"
+  #nav_title <- "CUSTOM INDICATORS REPORTING"
+  nav_title <- "FEPFAR/USAID - CUSTOM INDICATORS REPORTING"
 
 # Functions ----
 
@@ -14,51 +22,157 @@ library(tidyverse)
 # User Interface ----
 
   cirg_ui <- fluidPage(
-    # Title
-    titlePanel(title = app_title),
 
-    # Navigation Bar * Pages
+    # Navigation Bar & Pages
     navbarPage(
-      id = ",",
+      id = "mainMenu",
       fluid = TRUE,
+      collapsible = TRUE,
       title = nav_title,
-      theme = shinytheme("sandstone"),
+      position = "fixed-top",
+      theme = shinytheme("cerulean"),#cerulean, spacelab, yeti, sandstone
+      #footer = titlePanel(title = app_title),
+      tags$style(type="text/css",
+        "body {padding-top: 70px;}
+        .navbar-nav {float: right;}"),
+      # Home Page ----
       tabPanel(
         id = "menuHome",
         title = "Home",
-        titlePanel(title = "Welcome to CIRG"),
+        titlePanel(title = "Welcome to Custom Indicators Reporting Page"),
         mainPanel(
           textInput(inputId = "userName",
                     label = "What's your name?",
                     value = "world"),
           textOutput("greeting"),
-
+          htmltools::includeMarkdown("cirg_guidance.md")
         )
       ),
+      # Submission Page ----
       tabPanel(
         id = "menuSubmissions",
         title = "Submissions",
-        titlePanel(title = "Country/OU Data Submissions")
+        titlePanel(title = "OU / Country Data Submissions"),
+        wellPanel(
+          fluidRow(
+            tags$style(type="text/css", "body {padding-bottom: 50px;}"),
+            # 0: Input files ----
+            column(
+              width = 12,
+              fileInput(
+                inputId = "submRaw",
+                label = "Select CIRG Submission file(s) to start ...",
+                multiple = TRUE,
+                accept = ".xlsx",
+                width = NULL,
+                buttonLabel = "Select...",
+                placeholder = "No file selected"
+              )
+            ),
+            # 1: Process input files ----
+            column(
+              width = 12,
+              # Process files
+              actionButton(
+                inputId = "getSubmMeta",
+                label="Metadata",
+                icon = icon("info")
+              ),
+              # Read Contents
+              actionButton(
+                inputId = "importSubm",
+                label="Import",
+                icon = icon("table")
+              ),
+              # Augment Contents
+              actionButton(
+                inputId = "transformSubm",
+                label="Transform",
+                icon = icon("refresh")
+              ),
+              # Process files
+              actionButton(
+                inputId = "validateSubm",
+                label="Validate",
+                icon = icon("check")
+              ),
+              # Augment Contents
+              actionButton(
+                inputId = "augmentSubm",
+                label="Augment",
+                icon = icon("exchange")
+              ),
+              # Export Contents
+              actionButton(
+                inputId = "ingestSubm",
+                label="Ingest",
+                icon = icon("database")
+              ),
+              # Export Contents
+              actionButton(
+                inputId = "exportSubm",
+                label="Export",
+                icon = icon("share")
+              )
+            ),
+            # Submission notifications
+            column(
+              width = 12,
+              tags$style(type="text/css", "#submNotification {padding-top: 10px;}"),
+              uiOutput(outputId = "submNotification")
+            )
+          )
+        ),
+        # File Status
+        fluidRow(
+          column(
+            width = 12,
+            uiOutput(outputId = "submProcessResults"),
+            DT::dataTableOutput(outputId="submFilesList")
+          ),
+          column(
+            width = 12,
+            #tags$style(type="text/css", "#submFilesMeta {padding-top: 20px;}"),
+            DT::dataTableOutput(outputId="submFilesMeta")
+          ),
+          column(
+            width = 12,
+            uiOutput(outputId = "submProcessData"),
+            DT::dataTableOutput(outputId="submFilesData")
+          )
+        )
       ),
+      # Processing Page ----
       tabPanel(
         id = "menuProcessing",
         title = "Processing",
         titlePanel(title = "Data Validation & Processing")
       ),
+      # Reports Page ----
       tabPanel(
         id = "menuReports",
         title = "Reports",
         titlePanel(title = "Data Processing Reports")
       ),
+      # Data Page ----
       tabPanel(
         id = "menuData",
         title = "Data",
         titlePanel(title = "Processed Data")
       ),
-      tabPanel(
-        id = "menuHelp",
+      # Other Pages ----
+      navbarMenu(
         title = "Help-Q&A",
-        titlePanel(title = "Resources, Help & Q&A")
+        tabPanel(
+          id = "help",
+          title = "Help Resources",
+          titlePanel(title = "Help Resources")
+        ),
+        tabPanel(
+          id = "questions",
+          title = "Frequently Asked Questions",
+          titlePanel(title = "Frequently Asked Questions")
+        )
       )
     )
   )
@@ -67,19 +181,267 @@ library(tidyverse)
 
   cirg_server <- function(input, output, session) {
 
-    #message("The greeting is: ", output$greeting)
-
-    curr_date <- lubridate::ymd(Sys.Date())
-    #curr_date <- Sys.time()
-
-    #output$greeting <- renderText({curr_date()})
-
-    # output$greeting <- renderText({
-    #   paste0("Hello ", input$userName, "!")
-    # })
-
+    # Home Page ----
     output$greeting <- renderText({
-      paste0("Hello ", input$userName, "! Today's date is ", curr_date())
+      #curr_date <- lubridate::ymd(Sys.Date())
+      paste0("Hello ", input$userName, "! Today's date is ", Sys.Date())
+    })
+
+    # Submissions Page ----
+
+    subm_files <- NULL
+
+    # List of selected submissions
+    observeEvent(input$submRaw, {
+
+      if (is.null(input$submRaw)) return(NULL)
+
+      # Save for other processes
+      subm_files <<- input$submRaw
+
+      # Update header
+      output$submProcessResults <- renderUI(
+        HTML(as.character(h4("Selected submission(s) details")))
+      )
+
+      # Reformat files list for datatable
+      output$submFilesList <- DT::renderDataTable({
+        tbl_subm_files <- subm_files %>%
+          mutate(type = tools::file_ext(datapath),
+                 id = row_number()) %>%
+          select(-datapath) %>%
+          relocate(id, .before = 1) %>%
+          select(-id) # DataTable takes care of the row id
+
+        #return(subm_files)
+        DT::datatable(
+          tbl_subm_files,
+          colnames = str_to_upper(names(tbl_subm_files)),
+          height = "200px",
+          options = list(
+            dom = "t",
+            scrollX = TRUE,
+            scrollY = TRUE
+          )
+        )
+      })
+    })
+
+    # Meta data from selected file
+    df_metas <- NULL
+
+    observeEvent(input$getSubmMeta, {
+
+      if (is.null(subm_files)) {
+        # Error
+        output$submNotification <- renderUI({
+          HTML(as.character(p("ERROR - No submission file selected.",
+                              style = "color:red")))
+        })
+
+        # Clear validations title
+        output$submProcessResults <- renderUI({
+          HTML(as.character(h4("")))
+        })
+        # Clear data title
+        output$submProcessData <- renderUI({
+          HTML(as.character(h4("")))
+        })
+
+        return(NULL)
+      }
+
+      # Clear Error Messages
+      output$submNotification <- renderUI({HTML(as.character(p("")))})
+
+      # Update validations header
+      output$submProcessResults <- renderUI({
+        HTML(as.character(h4("Selected submission(s) initial validation results")))
+      })
+
+      # Clear data header
+      output$submProcessData <- renderUI({
+        HTML(as.character(h4("")))
+      })
+
+      # Overwrite file list
+      # Extract metadata and run initial validations
+      output$submFilesList <- DT::renderDataTable({
+
+        tbl_subm_files <- subm_files %>%
+          mutate(filename = basename(datapath)) %>%
+          select(filename, name, datapath)
+
+        # Initial Validations
+        metas <- tbl_subm_files %>%
+          pull(datapath) %>%
+          map_dfr(function(.file){
+
+            meta <- validate_initial(filepath = .file)
+
+            return(meta)
+          })
+
+        # Make metas available globally
+        df_metas <<- metas %>%
+          left_join(tbl_subm_files, by = "filename") %>%
+          mutate(filename = name) %>%
+          select(-datapath, -name)
+
+        i_files <- df_metas %>%
+          filter(!subm_valid) %>%
+          pull(filename)
+
+        if (length(i_files) > 0) {
+
+          fnames <- paste(i_files, collapse = ", ")
+
+          output$submNotification <- renderUI({
+            HTML(as.character(p(glue("WARNING - These submission file(s) have some errors and can not be imported: {fnames}"),
+                                style = glue("color:{glitr::burnt_sienna}"))))
+          })
+        }
+
+        # Return DataTable
+        DT::datatable(
+          df_metas,
+          colnames = str_to_upper(str_replace_all(names(df_metas), "_", " ")),
+          filter = "top",
+          height = "300px",
+          options = list(
+            dom = "tlpi",
+            scrollX = TRUE,
+            scrollY = TRUE
+          )
+        ) %>%
+          DT::formatStyle(
+            columns = "filename",
+            valueColumns = "subm_valid",
+            backgroundColor = DT::styleEqual(FALSE, glitr::burnt_sienna_light)
+          )
+      })
+
+      # Clear subm data table
+      output$submFilesData <- DT::renderDataTable({NULL})
+    })
+
+
+    # Imports Pre-validated Submissions
+
+    df_imports <- NULL
+
+    observeEvent(input$importSubm, {
+
+      if (is.null(df_metas)) {
+        output$submNotification <- renderUI({
+          HTML(as.character(p("ERROR - Make sure submission files are uploaded and pre-validated before proceeding with the ingestion.",
+                              style = "color:red")))
+        })
+        return(NULL)
+      }
+
+      output$submNotification <- renderUI({
+        HTML(as.character(p("")))
+      })
+
+      # Process data
+      df_ref <- subm_files %>%
+        mutate(datapath = basename(datapath)) %>%
+        select(name, datapath)
+
+      df_valids <- df_metas %>%
+        filter(subm_valid == TRUE) %>%
+        rename(subm_type = type) %>%
+        left_join(subm_files, by = c("filename" = "name")) %>%
+        select(-type, -size)
+
+      v_files <- df_valids %>% pull(filename)
+
+      if (length(v_files) > 0) {
+        output$submNotification <- renderUI({
+          HTML(as.character(p(glue("INFO - Files imported: {paste(v_files, collapse=', ')}"),
+                              style = glue("color:{glitr::genoa}"))))
+        })
+      } else {
+        output$submNotification <- renderUI({
+          HTML(as.character(p("ERROR - No valid submission to ingest. Try fixing the errors.",
+                              style = glue("color:{glitr::usaid_red}"))))
+        })
+        return(NULL)
+      }
+
+      # Update header - Checks
+      output$submProcessResults <- renderText({
+        HTML(as.character(h4("Selected submission(s) ingestion validation results")))
+      })
+
+      # Import data from submission files
+      df_imports <<- df_valids %>%
+        select(datapath, subm_type) %>%
+        pmap(~cir_import(filepath = .x, template = .y))
+
+      df_imports_checks <- df_imports %>%
+        map_dfr(function(.imp){
+          .imp$checks %>%
+            left_join(df_ref, by = c("filename" = "datapath")) %>%
+            select(-filename) %>%
+            select(filename = name, everything())
+        })
+
+      df_imports_data <- df_imports %>%
+        map_dfr(function(.imp){
+          .imp$data
+          # %>%
+          #   left_join(df_ref, by = c("filename" = "datapath")) %>%
+          #   select(-filename) %>%
+          #   select(filename = name, everything())
+        })
+
+      if (nrow(df_imports_data > 0)) {
+        # Update header - Data
+        output$submProcessData <- renderText({
+          HTML(as.character(h4("Selected submission(s) raw data")))
+        })
+      }
+      else {
+        output$submNotification <- renderUI({
+          HTML(as.character(p("ERROR - Unable to import data. See validations results for errors."),
+                            style = "color:red"))
+        })
+      }
+
+      #print(df_imports)
+      # glimpse(df_imports[[1]]$checks)
+      # glimpse(df_imports[[1]]$data)
+      print(length(df_imports))
+
+      output$submFilesList <- DT::renderDataTable({
+        DT::datatable(
+          df_imports_checks,
+          colnames = str_to_upper(str_replace_all(names(df_imports_checks), "_", " ")),
+          filter = "top",
+          height = "300px",
+          options = list(
+            dom = "tlpi",
+            scrollX = TRUE,
+            scrollY = "250px"
+          )
+        )
+      })
+
+      output$submFilesData <- DT::renderDataTable({
+        DT::datatable(
+          df_imports_data,
+          colnames = str_to_upper(str_replace_all(names(df_imports_data), "_", " ")),
+          filter = "top",
+          height = "300px",
+          options = list(
+            dom = "tlpi",
+            scrollX = TRUE,
+            scrollY = "300px"
+          )
+        )
+      })
     })
   }
 
---
+++ data-raw/cirg_guidance.md
@@ -0,0 +1,458 @@
+Purpose of Custom Data Collection
+---------------------------------
+
+The main goal of Custom Indicator Reporting (CIG) is to suppliment
+existing data,PEPFAR MER (Monitoring, Evaluation and Reporting) and HFR
+(High Frequency Reporting), with additional custom indicators designed
+to fully and effectively monitor and manage programs across all program
+areas and to improve efforts towards continuous program improvement.
+
+Data Collection Process
+-----------------------
+
+### List of Indicators
+
+A set of 28 custom indicators is requested for central collection and is
+organized by technical area. As with the MER indicators, most indicators
+are requested on a quarterly basis, with OVC indicators requested
+semi-annually. The list of indicators is included below, and full
+details for each indicator, including disaggregates and how to collect
+and use the data, can be found in the USAID PEPFAR Central Custom
+Indicators Reference Guide.
+
+Country teams should only submit indicators that are relevant to their
+existing PEPFAR program. For example, if PrEP activities are not
+supported by USAID in-country, the mission is not expected to report on
+the 5 PrEP indicators listed below. As this process evolves through
+initial implementation, feedback, and review, the list of indicators may
+be amended for future data collection cycles (reducing and/or adding key
+custom indicators or technical areas).
+
+#### Quarterly Indicators
+
+<table class="table table-striped" style="width: auto !important; ">
+<thead>
+<tr>
+<th style="text-align:left;">
+Technical Area
+</th>
+<th style="text-align:left;">
+Indicator
+</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td style="text-align:left;">
+DREAMS
+</td>
+<td style="text-align:left;">
+DREAMS\_FP
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+GENDER
+</td>
+<td style="text-align:left;">
+GEND\_GBV
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+GEND\_GBV\_OTHER
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+GEND\_LINK\_COMM
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+GEND\_NORM
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+KP
+</td>
+<td style="text-align:left;">
+TX\_NEW\_VERIFY
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+TX\_CURR\_VERIFY
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+TX\_PVLS\_VERIFY
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+TX\_RTT\_VERIFY
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+LAB
+</td>
+<td style="text-align:left;">
+TX\_PVLS\_ELIGIBLE
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+TX\_PVLS\_SAMPLE
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+TX\_PVLS\_RESULT\_RETURNED
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+PMTCT\_EID\_ELIGIBLE PMTCT\_EID\_RESULT\_RETURNED
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+PrEP
+</td>
+<td style="text-align:left;">
+PrEP\_OFFER
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+PrEP\_1MONTH
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+PrEP\_NEW\_VERIFY
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+PrEP\_CT\_VERIFY
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+SUPPLY CHAIN
+</td>
+<td style="text-align:left;">
+SC\_ARVDISP
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+SC\_LMIS
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+SC\_CURR
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+VMMC
+</td>
+<td style="text-align:left;">
+VMMC\_AE
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+OTHER
+</td>
+<td style="text-align:left;">
+TX\_CURR\_DART
+</td>
+</tr>
+</tbody>
+</table>
+
+#### Semi-Annual Indicators
+
+<table class="table table-striped" style="width: auto !important; ">
+<thead>
+<tr>
+<th style="text-align:left;">
+Technical Area
+</th>
+<th style="text-align:left;">
+Indicator
+</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td style="text-align:left;">
+OVC
+</td>
+<td style="text-align:left;">
+OVC\_OFFER
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+OVC\_ENROLL OVC\_VL\_ELIGIBLE (\<18)
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+OVC\_VLR (\<18)
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+OVC\_VLS (\<18)
+</td>
+</tr>
+</tbody>
+</table>
+
+#### Data Submission Templates
+
+All central custom data should be collected through the Excel-based
+template. The template is structured similarly to the HFR template, with
+three format options for submission (long , wide, semi-wide). Country
+teams can combine IP data into one template, or keep submissions from
+different partners and/or technical areas separate.
+
+Data Submission Process
+-----------------------
+
+### Timeline & Reporting Frequency
+
+The following table designates the reporting cycle for Custom Indicator
+Reporting. Each CIRG reporting period is a full fiscal quarter. Data
+submissions are due in alignment with the PEPFAR Data Calendar MER
+Initial Data Entry and Clean Data entry submission deadlines for DATIM.
+Submissions will be sent by the prime partner to the USAID country teams
+who will compile and submit through the Central Custom Indicator
+Submission Form. Late and re-submissions will be accepted on a rolling
+basis, however, only data submitted by the late and re-submission
+deadlines will be included in the CIRG dataset used for tools and
+products in that respective quarter. Data submitted after the
+late/re-submission deadline will be included in the next quarter's
+dataset.
+
+#### Reporting Calender
+
+<table class="table table-striped" style="width: auto !important; border-bottom: 0;">
+<thead>
+<tr>
+<th style="text-align:left;">
+Reporting Period
+</th>
+<th style="text-align:left;">
+Submission Type
+</th>
+<th style="text-align:left;">
+Due Dates
+</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td style="text-align:left;">
+FY23 Q1
+</td>
+<td style="text-align:left;">
+Initial
+</td>
+<td style="text-align:left;">
+February 10, 2023
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+Late/Re-submission\*
+</td>
+<td style="text-align:left;">
+March 17, 2023
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+FY23 Q2
+</td>
+<td style="text-align:left;">
+Initial
+</td>
+<td style="text-align:left;">
+May 12, 2023
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+Late/Re-submission\*
+</td>
+<td style="text-align:left;">
+June 16, 2023
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+FY23 Q3
+</td>
+<td style="text-align:left;">
+Initial
+</td>
+<td style="text-align:left;">
+August 11, 2023
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+Late/Re-submission\*
+</td>
+<td style="text-align:left;">
+September 15, 2023
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+FY23 Q4
+</td>
+<td style="text-align:left;">
+Initial
+</td>
+<td style="text-align:left;">
+November 10, 2023
+</td>
+</tr>
+<tr>
+<td style="text-align:left;">
+</td>
+<td style="text-align:left;">
+Late/Re-submission\*
+</td>
+<td style="text-align:left;">
+December 15, 2023
+</td>
+</tr>
+</tbody>
+<tfoot>
+<tr>
+<td style="padding: 0; " colspan="100%">
+[Note: ]{style="font-style: italic;"}
+</td>
+</tr>
+<tr>
+<td style="padding: 0; " colspan="100%">
+<sup></sup> \*Late/Re-submissions can be submitted at any time.
+</td>
+</tr>
+</tfoot>
+</table>
+
+#### Data Submission Format
+
+On or before the data submission date each quarter, Mission teams should
+submit completed data template(s) as attachment(s) to the Custom Data
+Submission Google Form.
+
+Template files should be named using the following format and should
+include partner and/or technical area if multiple templates are used.
+
+**CIRG\_FY\[YY\]*\[Period\]*\[OU\]*\[PARTNER\]*\[DATE SUBMITTED as
+YYYYMMDD\]**
+
+Eg:
+`CIRG_FY21_Q1_Moldova_20211015.xlsx or CIRG_FY21_Q1_Moldova_IP4data.20211015.xlsx`
+
+#### Common Reporting Errors
+
+Drag and drop errors for reporting periods, mechanism IDs, and orgunit
+UIDs resulting in consecutive numbers (e.g.- 11111, 11112, 11113). Any
+IDs/UIDs that do not align with IDs/UIDs referenced in DATIM will be
+excluded and flagged in the error report. Including multiple reporting
+periods in one file. This makes it difficult to remove data intended to
+be replaced by resubmitted data.
+
+Sending only the corrected rows instead of sending the entire original
+file with the corrected rows. When re-submitting data, it is best to
+resubmit the entire file with the corrected data to help us determine
+which old data to exclude.
+
+#### Non Custom Indicators
+
+Country teams and partners may routinely collect additional custom
+indicators that are not included in this Central Custom Indicator list.
+This list should not preclude Country teams from collecting these
+indicators independently. While Country teams and partners can submit
+additional data for non-custom indicators using the long template, this
+data will not be included in the final dataset for custom indicators.
